25b42aed3ea5571cdbf69df120af1361027a9e1f
neziw <kolopajlopiotr@gmail.com>
2023-02-02 18:23:05 +0100

    Implement vault methods

--- src/main/java/xyz/neziw/wallet/hooks/impl/VaultHook.java
+++ src/main/java/xyz/neziw/wallet/hooks/impl/VaultHook.java
@@ -1,12 +1,231 @@
 package xyz.neziw.wallet.hooks.impl;
 
+import lombok.AllArgsConstructor;
 import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import xyz.neziw.wallet.WalletPlugin;
+import xyz.neziw.wallet.utilities.DataUtils;
 
-public class VaultHook /*extends Economy*/ {
+import java.util.List;
+import java.util.UUID;
 
-    //@Override
+@AllArgsConstructor
+public class VaultHook extends Economy {
+
+    private final WalletPlugin plugin;
+
+    @Override
+    public EconomyResponse bankBalance(String name) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse bankDeposit(String name, double amount) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse bankHas(String name, double amount) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse bankWithdraw(String name, double amount) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse createBank(String name, OfflinePlayer player) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse createBank(String name, String player) {
+        return null;
+    }
+
+    @Override
+    public boolean createPlayerAccount(OfflinePlayer player) {
+        return false;
+    }
+
+    @Override
+    public boolean createPlayerAccount(OfflinePlayer player, String worldName) {
+        return false;
+    }
+
+    @Override
+    public boolean createPlayerAccount(String playerName) {
+        return false;
+    }
+
+    @Override
+    public boolean createPlayerAccount(String playerName, String worldName) {
+        return false;
+    }
+
+    @Override
+    public String currencyNamePlural() {
+        return "Balance"; // TODO - maybe configurable ? !
+    }
+
+    @Override
+    public String currencyNameSingular() {
+        return "Balance;"; // TODO - change this to something better
+    }
+
+    @Override
+    public EconomyResponse deleteBank(String name) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(String playerName, double amount) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(OfflinePlayer player, double amount) {
+        DataUtils.depositBalance(player.getName(), amount);
+        return new EconomyResponse(amount, getBalance(player), EconomyResponse.ResponseType.SUCCESS, "Success");
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(OfflinePlayer player, String worldName, double amount) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(String playerName, String worldName, double amount) {
+        return null;
+    }
+
+    @Override
+    public String format(double amount) {
+        return null;
+    }
+
+    @Override
+    public int fractionalDigits() {
+        return 0;
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer player) {
+        return DataUtils.getBalance(player.getName());
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer player, String world) {
+        return DataUtils.getBalance(player.getName());
+    }
+
+    @Override
+    public double getBalance(String playerName) {
+        return (Bukkit.getPlayer(playerName) != null) ? DataUtils.getBalance(playerName) : 0.0D;
+    }
+
+    @Override
+    public double getBalance(String playerName, String world) {
+        return 0;
+    }
+
+    @Override
+    public List<String> getBanks() {
+        return null;
+    }
+
+    @Override
+    public boolean has(String playerName, double amount) {
+        return false;
+    }
+
+    @Override
+    public boolean has(OfflinePlayer player, double amount) {
+        return DataUtils.getBalance(player.getName()) >= amount;
+    }
+
+    @Override
+    public boolean has(OfflinePlayer player, String worldName, double amount) {
+        return false;
+    }
+
+    @Override
+    public boolean has(String playerName, String worldName, double amount) {
+        return false;
+    }
+
+    @Override
+    public boolean hasAccount(OfflinePlayer player) {
+        return false;
+    }
+
+    @Override
+    public boolean hasAccount(OfflinePlayer player, String worldName) {
+        return false;
+    }
+
+    @Override
+    public boolean hasAccount(String playerName) {
+        return false;
+    }
+
+    @Override
+    public boolean hasAccount(String playerName, String worldName) {
+        return false;
+    }
+
+    @Override
     public boolean hasBankSupport() {
         return false;
     }
 
+    @Override
+    public EconomyResponse isBankMember(String name, OfflinePlayer player) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse isBankOwner(String name, OfflinePlayer player) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse isBankMember(String name, String playerName) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse isBankOwner(String name, String playerName) {
+        return null;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.plugin != null;
+    }
+
+    @Override
+    public EconomyResponse withdrawPlayer(String string, double amount) {
+        return withdrawPlayer(Bukkit.getPlayer(UUID.fromString(string)), amount);
+    }
+
+    @Override
+    public EconomyResponse withdrawPlayer(OfflinePlayer player, double amount) {
+        DataUtils.withDrawBalance(player.getName(), amount);
+        return (getBalance(player) >= amount ? new EconomyResponse(amount, getBalance(player), EconomyResponse.ResponseType.SUCCESS, "Success") : new EconomyResponse(amount, getBalance(player), EconomyResponse.ResponseType.FAILURE, "Error"));
+    }
+
+    @Override
+    public EconomyResponse withdrawPlayer(String playerName, String worldName, double amount) {
+        return null;
+    }
+
+    @Override
+    public EconomyResponse withdrawPlayer(OfflinePlayer player, String worldName, double amount) {
+        DataUtils.withDrawBalance(player.getName(), amount);
+        return (getBalance(player) >= amount ? new EconomyResponse(amount, getBalance(player), EconomyResponse.ResponseType.SUCCESS, "Success") : new EconomyResponse(amount, getBalance(player), EconomyResponse.ResponseType.FAILURE, "Error"));
+    }
 }
